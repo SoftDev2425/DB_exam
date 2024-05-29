5f5da261dc2879915a7b70cd23cd0ff5a4e13e14
Owais Dashti <owaisdashti@outlook.com>
2024-05-29 12:56:45 +0200

    search

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -5,6 +5,70 @@ import BookMetadata from "../models/bookmetdata.model";
 
 const booksRouter = Router();
 
+// Search for book (with pagination) query={searchTerm}&page={page}&limit={limit}
+booksRouter.get("/search", async (req: Request, res: Response) => {
+  try {
+    const searchTerm = req.query.query as string;
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+
+    // search for books
+    // const books = await BookMetadata.find({
+    //   $or: [
+    //     { title: { $regex: searchTerm, $options: "i" } },
+    //     { authors: { $regex: searchTerm, $options: "i" } },
+    //     { genres: { $regex: searchTerm, $options: "i" } },
+    //     { publisher: { $regex: searchTerm, $options: "i" } },
+    //   ],
+    // })
+    //   .limit(limit)
+    //   .skip(limit * (page - 1));
+
+    // Aggregation pipeline
+    const pipeline = [
+      {
+        $match: {
+          $or: [
+            { title: { $regex: searchTerm, $options: "i" } },
+            { authors: { $regex: searchTerm, $options: "i" } },
+            { genres: { $regex: searchTerm, $options: "i" } },
+            { publisher: { $regex: searchTerm, $options: "i" } },
+          ],
+        },
+      },
+      {
+        $facet: {
+          metadata: [{ $count: "total" }, { $addFields: { page, limit } }],
+          data: [{ $skip: limit * (page - 1) }, { $limit: limit }],
+        },
+      },
+      {
+        $project: {
+          total: { $arrayElemAt: ["$metadata.total", 0] },
+          page: 1,
+          limit: 1,
+          books: "$data",
+        },
+      },
+    ];
+
+    // search for books
+    const result = await BookMetadata.aggregate(pipeline);
+
+    const { total, books: data } = result[0];
+
+    res.status(200).json({
+      total,
+      page,
+      limit,
+      books: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
+
 // get book by isbn route
 booksRouter.get("/:isbn", async (req: Request, res: Response) => {
   try {
