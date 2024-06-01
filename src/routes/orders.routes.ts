b40e9ee4c49cd6f212878d4194c3285ff301efd6
rasm445f <90387951+rasm445f@users.noreply.github.com>
2024-06-01 11:14:51 +0200

    Procedure: Check for books that need restocking done

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,10 +1,59 @@
 import { Request, Response, Router } from "express";
 import { CustomRequest } from "../ICustomRequest";
+import { redisClient } from "../../redis/client";
 
+// take basket form redis and create order in db
+// subtract from stock in both db and redis
+
+// {
+//   "message": "Basket updated",
+//   "basket": {
+//       "books": [
+//           {
+//               "isbn": "000613868-3",
+//               "title": "Microcebus murinus",
+//               "format": "Hardcover",
+//               "language": "Dari",
+//               "authors": [
+//                   "Sallee Samweyes"
+//               ],
+//               "quantity": 0,
+//               "price": 400,
+//               "publisher": "Jabbersphere",
+//               "thumbnailUrl": "http://dummyimage.com/185x100.png/5fa2dd/ffffff"
+//           },
+//           {
+//               "isbn": "000493415-6",
+//               "title": "Ardea golieth",
+//               "format": "eBook",
+//               "language": "Armenian",
+//               "authors": [
+//                   "Arlee Coo"
+//               ],
+//               "quantity": 0,
+//               "price": 575,
+//               "publisher": "Photobean",
+//               "thumbnailUrl": "http://dummyimage.com/124x100.png/ff4444/ffffff"
+//           }
+//       ]
+//   }
+// }
+
+// New Router instance
 const orderRoutes = Router();
 
-orderRoutes.get("/", async (req: Request, res: Response) => {
+orderRoutes.get("/order", async (req: CustomRequest, res: Response) => {
   try {
+    const userId = req.userId;
+
+    const userBasket = redisClient.get(`basket-${userId}`);
+
+    if (!userBasket) {
+      return res.status(404).json({ message: "Basket not found!" });
+    }
+
+    console.log(userBasket);
+
     return res.status(200).json({ message: "Orders route" });
   } catch (error) {
     console.error(error);
